f11715d7d5bbd7b2bd108611b7f825bec96f0a7b
Lordeath19 <yrayhany@gmail.com>
2023-02-16 03:22:29 +0200

    Fixed Some planes to use more effective loadouts

--- resources/customized_payloads/A-10C_2.lua
+++ resources/customized_payloads/A-10C_2.lua
@@ -61,28 +61,32 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[3] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 10,
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 7,
 				},
 				[4] = {
 					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
 					["num"] = 8,
 				},
 				[5] = {
-					["CLSID"] = "{E6A6262A-CA08-4B3D-B030-E1A993B98453}",
-					["num"] = 9,
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 10,
 				},
 				[6] = {
-					["CLSID"] = "{E6A6262A-CA08-4B3D-B030-E1A993B98452}",
-					["num"] = 3,
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 5,
 				},
 				[7] = {
 					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
 					["num"] = 4,
 				},
 				[8] = {
-					["CLSID"] = "{LAU-131 - 7 AGR-20A}",
-					["num"] = 2,
+					["CLSID"] = "{DAC53A2F-79CA-42FF-A77A-F5649B601308}",
+					["num"] = 9,
+				},
+				[9] = {
+					["CLSID"] = "{DAC53A2F-79CA-42FF-A77A-F5649B601308}",
+					["num"] = 3,
 				},
 			},
 			["tasks"] = {

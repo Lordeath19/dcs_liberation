5e4470a31602a37c5234a98bda40907404ca1efc
Lordeath19 <yrayhany@gmail.com>
2023-02-20 20:14:39 +0200

    Barcap now preferred only on conflicts before airfields in range (AI will target conflict areas before random bases)

--- game/commander/objectivefinder.py
+++ game/commander/objectivefinder.py
@@ -139,12 +139,23 @@ class ObjectiveFinder:
         """Iterates over all active front lines in the theater."""
         yield from self.game.theater.conflicts()
 
+    def vulnerable_front_lines(self) -> Iterator[ControlPoint]:
+        """Iterates over friendly CPs that aren't behind friendly lines"""
+        for cp in self.friendly_control_points():
+            if isinstance(cp, OffMapSpawn):
+                continue
+            if cp.has_active_frontline or cp.is_isolated:
+                yield cp
+
     def vulnerable_control_points(self) -> Iterator[ControlPoint]:
         """Iterates over friendly CPs that are vulnerable to enemy CPs.
 
-        Vulnerability is defined as any enemy CP within threat range of of the
+        Vulnerability is defined as any enemy CP within threat range of the
         CP.
         """
+        if list(self.game.theater.conflicts()):
+            yield from self.vulnerable_front_lines()
+            return
         for cp in self.friendly_control_points():
             if isinstance(cp, OffMapSpawn):
                 # Off-map spawn locations don't need protection.
@@ -157,10 +168,6 @@ class ObjectiveFinder:
             )
             for airfield in airfields_in_threat_range:
                 if not airfield.is_friendly(self.is_player):
-                    yield cp
-                    return
-            for conflict in self.game.theater.conflicts():
-                if cp in conflict.control_points:
                     yield cp
 
     def oca_targets(self, min_aircraft: int) -> Iterator[ControlPoint]:

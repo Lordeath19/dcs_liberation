02289dbf01ad81199d2fde8f7e660fec5862c89b
Lordeath19 <yrayhany@gmail.com>
2023-02-20 19:59:51 +0200

    B-52 now uses cruise missiles as default attack instead of regular bombs

--- resources/customized_payloads/B-52H.lua
+++ resources/customized_payloads/B-52H.lua
@@ -65,12 +65,16 @@ local unitPayloads = {
 			["name"] = "CAS",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{4CD2BB0F-5493-44EF-A927-9760350F7BA1}",
-					["num"] = 3,
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{4CD2BB0F-5493-44EF-A927-9760350F7BA1}",
-					["num"] = 1,
+					["CLSID"] = "{8DCAF3A3-7FCF-41B8-BB88-58DEDA878EDE}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{45447F82-01B5-4029-A572-9AAD28AF0275}",
+					["num"] = 3,
 				},
 			},
 			["tasks"] = {

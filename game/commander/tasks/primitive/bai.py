c9638b4158b2d44e2f7a2af9c47eb9854c84ca4b
Lordeath19 <yrayhany@gmail.com>
2023-04-19 21:19:24 +0300

    BAI strikes now start as CAS AI have smart target priority when dealing with cas

--- game/commander/tasks/primitive/bai.py
+++ game/commander/tasks/primitive/bai.py
@@ -21,5 +21,5 @@ class PlanBai(PackagePlanningTask[VehicleGroupGroundObject]):
         state.eliminate_battle_position(self.target)
 
     def propose_flights(self) -> None:
-        self.propose_flight(FlightType.BAI, 2)
+        self.propose_flight(FlightType.CAS, 2)
         self.propose_common_escorts()

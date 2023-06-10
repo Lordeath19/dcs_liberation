3a52e6180b0d126f4adc0d4e8be0e1f8efdd2a90
Lordeath19 <yrayhany@gmail.com>
2023-06-10 15:57:26 +0300

    Updated loadouts to use optimal weapons for AI

--- resources/customized_payloads/B-1B.lua
+++ resources/customized_payloads/B-1B.lua
@@ -2,41 +2,38 @@ local unitPayloads = {
 	["name"] = "B-1B",
 	["payloads"] = {
 		[1] = {
-			["displayName"] = "Liberation Strike",
-			["name"] = "Liberation Strike",
+			["name"] = "SEAD",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 3,
 				},
 			},
 			["tasks"] = {
-				[1] = 34,
-				[2] = 32,
+				[1] = 33,
 			},
 		},
 		[2] = {
-			["displayName"] = "Liberation OCA/Aircraft",
-			["name"] = "Liberation OCA/Aircraft",
+			["name"] = "STRIKE",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "GBU-31V3B*8",
+					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
 					["num"] = 3,
 				},
 			},
@@ -46,56 +43,6 @@ local unitPayloads = {
 			},
 		},
 		[3] = {
-			["name"] = "CAS",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "CBU97*10",
-					["num"] = 3,
-				},
-				[2] = {
-					["CLSID"] = "CBU97*10",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "CBU97*10",
-					["num"] = 1,
-				},
-			},
-			["tasks"] = {
-				[1] = 33,
-			},
-		},
-		[4] = {
-			["name"] = "CAP",
-			["pylons"] = {
-			},
-			["tasks"] = {
-				[1] = 33,
-			},
-		},
-		[5] = {
-			["displayName"] = "Liberation OCA/Runway",
-			["name"] = "Liberation OCA/Runway",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "GBU-31V3B*8",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "GBU-31V3B*8",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "GBU-31V3B*8",
-					["num"] = 3,
-				},
-			},
-			["tasks"] = {
-				[1] = 34,
-				[2] = 32,
-			},
-		},
-		[6] = {
 			["name"] = "ANTISHIP",
 			["pylons"] = {
 				[1] = {
@@ -115,74 +62,16 @@ local unitPayloads = {
 				[1] = 33,
 			},
 		},
-		[7] = {
-			["displayName"] = "Liberation CAS",
-			["name"] = "Liberation CAS",
+		[4] = {
+			["name"] = "CAP",
 			["pylons"] = {
-				[1] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 3,
-				},
 			},
 			["tasks"] = {
-				[1] = 34,
-				[2] = 32,
+				[1] = 33,
 			},
 		},
-		[8] = {
-			["displayName"] = "Liberation DEAD",
-			["name"] = "Liberation DEAD",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
-					["num"] = 3,
-				},
-			},
-			["tasks"] = {
-				[1] = 34,
-				[2] = 32,
-			},
-		},
-		[9] = {
-			["displayName"] = "Liberation BAI",
-			["name"] = "Liberation BAI",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 1,
-				},
-				[2] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 2,
-				},
-				[3] = {
-					["CLSID"] = "GBU-31*8",
-					["num"] = 3,
-				},
-			},
-			["tasks"] = {
-				[1] = 34,
-				[2] = 32,
-			},
-		},
-		[10] = {
-			["name"] = "SEAD",
+		[5] = {
+			["name"] = "CAS",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{AABA1A14-78A1-4E85-94DD-463CF75BD9E4}",
